1c395c812404717f2c2a76c059ee4be7f645ae4a
Javier Auspont Cerda <auspont.javier@gmail.com>
2023-09-06 11:32:19 -0300

    login_basico

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -7,15 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  token = 'hola123asupont123'
+  username: string = '';
+  password: string = '';
+  usuariosLista: any[] = [];
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  registrar(){
-    localStorage.setItem('token',this.token)
+  registrar() {
+    // Validar que se hayan ingresado datos en los campos
+    if (this.username && this.password) {
+      // Crear un objeto con los datos ingresados
+      const userData = {
+        username: this.username,
+        password: this.password,
+      };
+
+      // Agregar el objeto a la lista
+      this.usuariosLista.push(userData);
+
+      // Guardar la lista en el localStorage
+      localStorage.setItem('users', JSON.stringify(this.usuariosLista));
+
+      // Limpiar los campos de entrada
+      this.username = '';
+      this.password = '';
+    }
   }
 
 }
