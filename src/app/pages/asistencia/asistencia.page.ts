01a4a723ec7ae1c1873a1876b8c88f14f5b120f1
frenchpig <auspont.javier@gmail.com>
2023-09-12 15:18:51 -0300

    qr scanner 100%

--- src/app/pages/asistencia/asistencia.page.ts
+++ src/app/pages/asistencia/asistencia.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-asistencia',
@@ -10,13 +11,31 @@ export class AsistenciaPage implements OnInit {
 
   dataList: any[] = []
 
-  constructor(private alertController: AlertController) { }
+  constructor(private alertController: AlertController,private router: Router) { }
 
   ngOnInit() {
     let dataJSON = localStorage.getItem('asistencia');
     if (dataJSON) {
       this.dataList = JSON.parse(dataJSON);
     }
+    this.mostrarExitoso();
+  }
+
+  ngOnDestroy(){
+    localStorage.removeItem('asistencia');
+  }
+
+  backHome(){
+    this.router.navigate(['/login']);
+  }
+
+  async mostrarExitoso() {
+    const alert = await this.alertController.create({
+      header: 'Quedaste Presente!',
+      message: 'Asistencia Registrada con exito!',
+      buttons: ['Aceptar']
+    });
+    await alert.present();
   }
 
 }
